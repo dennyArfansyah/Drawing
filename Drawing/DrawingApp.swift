c99ea7206995bd4ebf5fc16b3388fa93376bcb46
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-19 14:27:44 +0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Denny Arfansyah on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
